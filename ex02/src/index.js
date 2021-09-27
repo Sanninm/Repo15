284c6f4f6b9e4f2c319e98f14ccdb3c13eeb142d
Sanninm <mudzelet.sanin@gmail.com>
2021-09-27 15:37:27 +0200

    ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,20 @@
+function checkVariableScope() {
+    'use struct';
+
+    // Only change code below this line
+    var i = 'function variable';
+    // Only change code above this line
+
+    if (true) {
+        // only change code below this line
+        i = 'block variable';
+        // only change code above this line
+
+        console.log('Scope i is: ', i);
+    }
+    console.log('Scope i is: ', i);
+    return i;
+}
+
+checkVariableScope();
+module.exports = checkVariableScope;
