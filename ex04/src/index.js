6cf0b49642e0c678aee39ab8e0f417c03934bef1
Sanninm <mudzelet.sanin@gmail.com>
2021-09-28 10:21:50 +0200

    ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+const milili = [10, 25, 4]; // DO NOT change this line
+
+function myArr(mili) {
+    'use strict';
+
+// Only change code below this line
+
+// Using mili = [4, 10, 25] would be invalid
+
+// Only change code above this line
+return mili;
+}
+console.log(maArr(5));
+module.exports = myArr;
